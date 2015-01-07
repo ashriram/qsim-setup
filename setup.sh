cd60ac562077b30bd6ef7f9cbb7f7db45d5f6335
Pranith Kumar <bobby.prani@gmail.com>
2015-01-07 15:04:39 -0500

    Untar new arm images

--- setup.sh
+++ setup.sh
@@ -45,9 +45,9 @@ cp libqsim.so lib/
 echo "Downloading arm QEMU images..."
 # wget https://www.dropbox.com/s/wtie9kghc95em7o/qsim_arm_images.tar.bz2?dl=0
 wget https://www.dropbox.com/s/ekglfaqogewrojl/arm_images.tar.bz2?dl=0
-# cp ~/devops/Dropbox/qsim-ARM/qsim_arm_images.tar.bz2 .
-tar -xjvf qsim_arm_images.tar.bz2
-tar -xvf qsim_arm_images.tar
+# cp ~/devops/Dropbox/qsim-ARM/arm_images.tar.bz2 .
+tar -xjvf arm_images.tar.bz2
+tar -xvf arm_images.tar
 
 # run simple example
 echo "Running the simple example..."
